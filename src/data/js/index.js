009f8e91fc072fa0f16db68baa42cb9dff9c8999
Wesley DuBose <wsd10205@gmail.com>
2021-11-15 02:01:31 -0500

    Finalized Upload

---
+++ src/data/js/index.js
@@ -0,0 +1,476 @@
+var today = new Date().getHours();
+    if (today >= 0 && today <= 1) {
+    document.getElementById("timeBlk1").style.background = "Aquamarine";
+    } else {
+        document.getElementById("timeBlk1").style.background = "lightgrey";
+    }
+
+    if (today >= 1 && today <= 2) {
+        document.getElementById("timeBlk2").style.background = "Aquamarine";
+    } if (today >= 0 && today <= 1) {
+        document.getElementById("timeBlk2").style.background = "lightgrey";
+    } else {
+        document.getElementById("timeBlk2").style.background = "darkgrey";
+    }
+
+    if (today >= 2 && today <= 3) {
+        document.getElementById("timeBlk3").style.background = "Aquamarine";
+     } if (today >= 0 && today <= 2) {
+        document.getElementById("timeBlk3").style.background = "lightgrey";
+     } else {
+         document.getElementById("timeBlk3").style.background = "darkgrey";
+     }
+
+     if (today >= 3 && today <= 4) {
+        document.getElementById("timeBlk3").style.background = "Aquamarine";
+    } if (today >= 0 && today <= 2) {
+        document.getElementById("timeBlk3").style.background = "lightgrey";
+    } else {
+        document.getElementById("timeBlk3").style.background = "darkgrey";
+    }
+
+    if (today >= 4 && today <= 5) {
+        document.getElementById("timeBlk4").style.background = "Aquamarine";
+     } if (today >= 0 && today <= 3) {
+        document.getElementById("timeBlk4").style.background = "lightgrey";
+     } else {
+         document.getElementById("timeBlk4").style.background = "darkgrey";
+     }
+     
+     if (today >= 5 && today <= 6) {
+        document.getElementById("timeBlk5").style.background = "Aquamarine";
+    } if (today >= 0 && today <= 4) {
+        document.getElementById("timeBlk5").style.background = "lightgrey";
+    } else {
+        document.getElementById("timeBlk5").style.background = "darkgrey";
+    }
+
+    if (today >= 6 && today <= 7) {
+        document.getElementById("timeBlk6").style.background = "Aquamarine";
+     } if (today >= 0 && today <= 5) {
+        document.getElementById("timeBlk6").style.background = "lightgrey";
+     } else {
+         document.getElementById("timeBlk6").style.background = "darkgrey";
+     }
+
+     if (today >= 7 && today <= 8) {
+        document.getElementById("timeBlk7").style.background = "Aquamarine";
+    } if (today >= 0 && today <= 6) {
+        document.getElementById("timeBlk7").style.background = "lightgrey";
+    } else {
+        document.getElementById("timeBlk7").style.background = "darkgrey";
+    }
+
+    if (today >= 8 && today <= 9) {
+        document.getElementById("timeBlk8").style.background = "Aquamarine";
+     } if (today >= 0 && today <= 7) {
+        document.getElementById("timeBlk8").style.background = "lightgrey";
+     } else {
+         document.getElementById("timeBlk8").style.background = "darkgrey";
+     }
+
+     if (today >= 9 && today <= 10) {
+        document.getElementById("timeBlk9").style.background = "Aquamarine";
+    } if (today >= 0 && today <= 8) {
+        document.getElementById("timeBlk9").style.background = "lightgrey";
+    } else {
+        document.getElementById("timeBlk9").style.background = "darkgrey";
+    }
+
+    if (today >= 10 && today <= 11) {
+        document.getElementById("timeBlk10").style.background = "Aquamarine";
+     } if (today >= 0 && today <= 9) {
+        document.getElementById("timeBlk10").style.background = "lightgrey";
+     } else {
+         document.getElementById("timeBlk10").style.background = "darkgrey";
+     }
+
+     if (today >= 11 && today <= 12) {
+        document.getElementById("timeBlk11").style.background = "Aquamarine";
+    } if (today >= 0 && today <= 10) {
+        document.getElementById("timeBlk11").style.background = "lightgrey";
+    } else {
+        document.getElementById("timeBlk11").style.background = "darkgrey";
+    }
+
+    if (today >= 12 && today <= 13) {
+        document.getElementById("timeBlk12").style.background = "Aquamarine";
+     } if (today >= 0 && today <= 11) {
+        document.getElementById("timeBlk12").style.background = "lightgrey";
+     } else {
+         document.getElementById("timeBlk12").style.background = "darkgrey";
+     }
+     
+     if (today >= 13 && today <= 14) {
+        document.getElementById("timeBlk13").style.background = "Aquamarine";
+    } if (today >= 0 && today <= 12) {
+        document.getElementById("timeBlk13").style.background = "lightgrey";
+    } else {
+        document.getElementById("timeBlk13").style.background = "darkgrey";
+    }
+
+    if (today >= 14 && today <= 15) {
+        document.getElementById("timeBlk14").style.background = "Aquamarine";
+     } if (today >= 0 && today <= 13) {
+        document.getElementById("timeBlk14").style.background = "lightgrey";
+     } else {
+         document.getElementById("timeBlk14").style.background = "darkgrey";
+     }
+
+     if (today >= 15 && today <= 16) {
+        document.getElementById("timeBlk15").style.background = "Aquamarine";
+    } if (today >= 0 && today <= 14) {
+        document.getElementById("timeBlk15").style.background = "lightgrey";
+    } else {
+        document.getElementById("timeBlk15").style.background = "darkgrey";
+    }
+
+    if (today >= 16 && today <= 17) {
+        document.getElementById("timeBlk16").style.background = "Aquamarine";
+     } if (today >= 0 && today <= 15) {
+        document.getElementById("timeBlk16").style.background = "lightgrey";
+     } else {
+         document.getElementById("timeBlk16").style.background = "darkgrey";
+     }
+
+     if (today >= 17 && today <= 18) {
+        document.getElementById("timeBlk17").style.background = "Aquamarine";
+    } if (today >= 0 && today <= 16) {
+        document.getElementById("timeBlk17").style.background = "lightgrey";
+    } else {
+        document.getElementById("timeBlk17").style.background = "darkgrey";
+    }
+
+    if (today >= 18 && today <= 19) {
+        document.getElementById("timeBlk18").style.background = "Aquamarine";
+     } if (today >= 0 && today <= 17) {
+        document.getElementById("timeBlk18").style.background = "lightgrey";
+     } else {
+         document.getElementById("timeBlk18").style.background = "darkgrey";
+     }
+
+     if (today >= 19 && today <= 20) {
+        document.getElementById("timeBlk19").style.background = "Aquamarine";
+    } if (today >= 0 && today <= 18) {
+        document.getElementById("timeBlk19").style.background = "lightgrey";
+    } else {
+        document.getElementById("timeBlk19").style.background = "darkgrey";
+    }
+
+    if (today >= 20 && today <= 21) {
+        document.getElementById("timeBlk20").style.background = "Aquamarine";
+     } if (today >= 0 && today <= 19) {
+        document.getElementById("timeBlk20").style.background = "lightgrey";
+     } else {
+         document.getElementById("timeBlk20").style.background = "darkgrey";
+     }
+
+     if (today >= 21 && today <= 22) {
+        document.getElementById("timeBlk21").style.background = "Aquamarine";
+    } if (today >= 0 && today <= 20) {
+        document.getElementById("timeBlk21").style.background = "lightgrey";
+    } else {
+        document.getElementById("timeBlk21").style.background = "darkgrey";
+    }
+
+    if (today >= 22 && today <= 23) {
+        document.getElementById("timeBlk22").style.background = "Aquamarine";
+     } if (today >= 0 && today <= 21) {
+        document.getElementById("timeBlk22").style.background = "lightgrey";
+     } else {
+         document.getElementById("timeBlk22").style.background = "darkgrey";
+     }
+
+     if (today >= 23 && today <= 24) {
+        document.getElementById("timeBlk23").style.background = "Aquamarine";
+    } if (today >= 0 && today <= 22) {
+        document.getElementById("timeBlk23").style.background = "lightgrey";
+    } else {
+        document.getElementById("timeBlk23").style.background = "darkgrey";
+    }
+
+    if (today >= 24) {
+        document.getElementById("timeBlk24").style.background = "Aquamarine";
+     } if (today >= 0 && today <= 23) {
+        document.getElementById("timeBlk24").style.background = "lightgrey";
+     } else {
+         document.getElementById("timeBlk24").style.background = "darkgrey";
+     }
+
+function save1 () {
+    var event1 = document.getElementById("event1").value;
+    if (typeof(Storage) !== "undefined") {
+        // Store
+        localStorage.setItem("localEvent1", event1);
+    } else {
+        document.getElementById("eventResult1").innerHTML = "Sorry, your browser does not support Web Storage...";
+    }
+}
+function save2 () {
+    var event2 = document.getElementById("event2").value;
+    if (typeof(Storage) !== "undefined") {
+        // Store
+        localStorage.setItem("localEvent2", event2);
+    } else {
+        document.getElementById("eventResult2").innerHTML = "Sorry, your browser does not support Web Storage...";
+    }
+}function save3 () {
+    var event3 = document.getElementById("event3").value;
+    if (typeof(Storage) !== "undefined") {
+        // Store
+        localStorage.setItem("localEvent3", event3);
+    } else {
+        document.getElementById("eventResult3").innerHTML = "Sorry, your browser does not support Web Storage...";
+    }
+}function save4 () {
+    var event4 = document.getElementById("event4").value;
+    if (typeof(Storage) !== "undefined") {
+        // Store
+        localStorage.setItem("localEvent4", event4);
+    } else {
+        document.getElementById("eventResult4").innerHTML = "Sorry, your browser does not support Web Storage...";
+    }
+}function save5 () {
+    var event5 = document.getElementById("event5").value;
+    if (typeof(Storage) !== "undefined") {
+        // Store
+        localStorage.setItem("localEvent5", event5);
+    } else {
+        document.getElementById("eventResult5").innerHTML = "Sorry, your browser does not support Web Storage...";
+    }
+}function save6 () {
+    var event6 = document.getElementById("event6").value;
+    if (typeof(Storage) !== "undefined") {
+        // Store
+        localStorage.setItem("localEvent6", event6);
+    } else {
+        document.getElementById("eventResult6").innerHTML = "Sorry, your browser does not support Web Storage...";
+    }
+}function save7 () {
+    var event7 = document.getElementById("event7").value;
+    if (typeof(Storage) !== "undefined") {
+        // Store
+        localStorage.setItem("localEvent7", event7);
+    } else {
+        document.getElementById("eventResult7").innerHTML = "Sorry, your browser does not support Web Storage...";
+    }
+}function save8 () {
+    var event8 = document.getElementById("event8").value;
+    if (typeof(Storage) !== "undefined") {
+        // Store
+        localStorage.setItem("localEvent8", event8);
+    } else {
+        document.getElementById("eventResult8").innerHTML = "Sorry, your browser does not support Web Storage...";
+    }
+}function save9 () {
+    var event9 = document.getElementById("event9").value;
+    if (typeof(Storage) !== "undefined") {
+        // Store
+        localStorage.setItem("localEvent9", event9);
+    } else {
+        document.getElementById("eventResult9").innerHTML = "Sorry, your browser does not support Web Storage...";
+    }
+}function save10 () {
+    var event10 = document.getElementById("event10").value;
+    if (typeof(Storage) !== "undefined") {
+        // Store
+        localStorage.setItem("localEvent10", event10);
+    } else {
+        document.getElementById("eventResult10").innerHTML = "Sorry, your browser does not support Web Storage...";
+    }
+}function save11 () {
+    var event11 = document.getElementById("event11").value;
+    if (typeof(Storage) !== "undefined") {
+        // Store
+        localStorage.setItem("localEvent11", event11);
+    } else {
+        document.getElementById("eventResult11").innerHTML = "Sorry, your browser does not support Web Storage...";
+    }
+}function save12 () {
+    var event12 = document.getElementById("event12").value;
+    if (typeof(Storage) !== "undefined") {
+        // Store
+        localStorage.setItem("localEvent12", event12);
+    } else {
+        document.getElementById("eventResult12").innerHTML = "Sorry, your browser does not support Web Storage...";
+    }
+}function save13 () {
+    var event13 = document.getElementById("event13").value;
+    if (typeof(Storage) !== "undefined") {
+        // Store
+        localStorage.setItem("localEvent13", event13);
+    } else {
+        document.getElementById("eventResult13").innerHTML = "Sorry, your browser does not support Web Storage...";
+    }
+}function save14 () {
+    var event14 = document.getElementById("event14").value;
+    if (typeof(Storage) !== "undefined") {
+        // Store
+        localStorage.setItem("localEvent14", event14);
+    } else {
+        document.getElementById("eventResult14").innerHTML = "Sorry, your browser does not support Web Storage...";
+    }
+}function save15 () {
+    var event15 = document.getElementById("event15").value;
+    if (typeof(Storage) !== "undefined") {
+        // Store
+        localStorage.setItem("localEvent15", event15);
+    } else {
+        document.getElementById("eventResult15").innerHTML = "Sorry, your browser does not support Web Storage...";
+    }
+}function save16 () {
+    var event16 = document.getElementById("event16").value;
+    if (typeof(Storage) !== "undefined") {
+        // Store
+        localStorage.setItem("localEvent16", event16);
+    } else {
+        document.getElementById("eventResult16").innerHTML = "Sorry, your browser does not support Web Storage...";
+    }
+}function save17 () {
+    var event17 = document.getElementById("event17").value;
+    if (typeof(Storage) !== "undefined") {
+        // Store
+        localStorage.setItem("localEvent17", event17);
+    } else {
+        document.getElementById("eventResult17").innerHTML = "Sorry, your browser does not support Web Storage...";
+    }
+}function save18 () {
+    var event18 = document.getElementById("event18").value;
+    if (typeof(Storage) !== "undefined") {
+        // Store
+        localStorage.setItem("localEvent18", event18);
+    } else {
+        document.getElementById("eventResult18").innerHTML = "Sorry, your browser does not support Web Storage...";
+    }
+}function save19 () {
+    var event19 = document.getElementById("event19").value;
+    if (typeof(Storage) !== "undefined") {
+        // Store
+        localStorage.setItem("localEvent19", event19);
+    } else {
+        document.getElementById("eventResult19").innerHTML = "Sorry, your browser does not support Web Storage...";
+    }
+}function save20 () {
+    var event20 = document.getElementById("event20").value;
+    if (typeof(Storage) !== "undefined") {
+        // Store
+        localStorage.setItem("localEvent20", event20);
+    } else {
+        document.getElementById("eventResult20").innerHTML = "Sorry, your browser does not support Web Storage...";
+    }
+}function save21 () {
+    var event21 = document.getElementById("event21").value;
+    if (typeof(Storage) !== "undefined") {
+        // Store
+        localStorage.setItem("localEvent21", event21);
+    } else {
+        document.getElementById("eventResult21").innerHTML = "Sorry, your browser does not support Web Storage...";
+    }
+}function save22 () {
+    var event22 = document.getElementById("event22").value;
+    if (typeof(Storage) !== "undefined") {
+        // Store
+        localStorage.setItem("localEvent22", event22);
+    } else {
+        document.getElementById("eventResult22").innerHTML = "Sorry, your browser does not support Web Storage...";
+    }
+}function save23 () {
+    var event23 = document.getElementById("event23").value;
+    if (typeof(Storage) !== "undefined") {
+        // Store
+        localStorage.setItem("localEvent23", event23);
+    } else {
+        document.getElementById("eventResult23").innerHTML = "Sorry, your browser does not support Web Storage...";
+    }
+}function save24 () {
+    var event24 = document.getElementById("event24").value;
+    if (typeof(Storage) !== "undefined") {
+        // Store
+        localStorage.setItem("localEvent24", event24);
+    } else {
+        document.getElementById("eventResult24").innerHTML = "Sorry, your browser does not support Web Storage...";
+    }
+}
+
+function showEvents () {
+
+    var show1 = localStorage.getItem("localEvent1");
+    document.getElementById("event1").value = show1;
+
+    var show2 = localStorage.getItem("localEvent2");
+    document.getElementById("event2").value = show2;
+
+    var show3 = localStorage.getItem("localEvent3");
+    document.getElementById("event3").value = show3;
+
+    var show4 = localStorage.getItem("localEvent4");
+    document.getElementById("event4").value = show4;
+
+    var show5 = localStorage.getItem("localEvent5");
+    document.getElementById("event5").value = show5;
+
+    var show6 = localStorage.getItem("localEvent6");
+    document.getElementById("event6").value = show6;
+
+    var show7 = localStorage.getItem("localEvent7");
+    document.getElementById("event7").value = show7;
+
+    var show8 = localStorage.getItem("localEvent8");
+    document.getElementById("event8").value = show8;
+
+    var show8 = localStorage.getItem("localEvent8");
+    document.getElementById("event8").value = show8;
+
+    var show9 = localStorage.getItem("localEvent9");
+    document.getElementById("event9").value = show9;
+
+    var show9 = localStorage.getItem("localEvent9");
+    document.getElementById("event9").value = show9;
+
+    var show10 = localStorage.getItem("localEvent10");
+    document.getElementById("event10").value = show10;
+
+    var show11 = localStorage.getItem("localEvent11");
+    document.getElementById("event11").value = show11;
+
+    var show12 = localStorage.getItem("localEvent12");
+    document.getElementById("event12").value = show12;
+
+    var show13 = localStorage.getItem("localEvent13");
+    document.getElementById("event13").value = show13;
+
+    var show14 = localStorage.getItem("localEvent14");
+    document.getElementById("event14").value = show14;
+
+    var show15 = localStorage.getItem("localEvent15");
+    document.getElementById("event15").value = show15;
+
+    var show16 = localStorage.getItem("localEvent16");
+    document.getElementById("event16").value = show16;
+
+    var show17 = localStorage.getItem("localEvent17");
+    document.getElementById("event17").value = show17;
+
+    var show18 = localStorage.getItem("localEvent18");
+    document.getElementById("event18").value = show18;
+
+    var show19 = localStorage.getItem("localEvent19");
+    document.getElementById("event19").value = show19;
+
+    var show20 = localStorage.getItem("localEvent20");
+    document.getElementById("event20").value = show20;
+
+    var show21 = localStorage.getItem("localEvent21");
+    document.getElementById("event21").value = show21;
+
+    var show22 = localStorage.getItem("localEvent22");
+    document.getElementById("event22").value = show22;
+
+    var show23 = localStorage.getItem("localEvent23");
+    document.getElementById("event23").value = show23;
+
+    var show24 = localStorage.getItem("localEvent24");
+    document.getElementById("event24").value = show24;
+
+    alert("Now showing all events saved in your local storage.");
+}
